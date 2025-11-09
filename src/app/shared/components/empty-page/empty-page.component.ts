a0a198ddc53ca6764cdfc764689b01eec2a14e39
101pacman <shakyaanuj818@gmail.com>
2025-11-09 18:26:39 +0545

    add experience detail

---
+++ src/app/shared/components/empty-page/empty-page.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-empty-page',
+  imports: [],
+  templateUrl: './empty-page.component.html',
+  styleUrl: './empty-page.component.css',
+})
+export class EmptyPageComponent {
+}
