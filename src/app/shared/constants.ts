86f78edfd950f9644b1dd9561d15770f3e9d5fa0
101pacman <shakyaanuj818@gmail.com>
2025-11-14 22:34:42 +0545

    refactor: fix header in place and empty dark image

--- src/app/shared/constants.ts
+++ src/app/shared/constants.ts
@@ -36,7 +36,7 @@ export const IMAGE_URLS = {
   profile: './assets/images/101pp.png',
   spotify: './assets/images/spotify.svg',
   emptyPageLight: './assets/images/empty-light.png',
-  emptyPageDark: './assets/images/empty-dark.png',
+  emptyPageDark: './assets/images/empty-dark-3.png',
   notFoundPageLight: './assets/images/not-found-page-light.svg',
   notFoundPageDark: './assets/images/not-found-page-dark-2.svg',
 }
