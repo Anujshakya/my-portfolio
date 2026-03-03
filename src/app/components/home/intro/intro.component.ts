933a2f1e2142e40591012751d1d79d530d4991cd
101pacman <shakyaanuj818@gmail.com>
2026-03-03 22:32:21 +0545

    first draft

--- src/app/components/home/intro/intro.component.ts
+++ src/app/components/home/intro/intro.component.ts
@@ -1,7 +1,10 @@
-import {Component} from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
 import {ProfileImageComponent, SpotifyCurrentPlayingComponent} from '../../../shared';
 import {LucideAngularModule} from 'lucide-angular';
 import {APP_CONSTANTS} from '../../../shared';
+import {Subscription} from 'rxjs';
+import {AboutMeModel} from '../../../models';
+import {AboutMeService} from '../../../services/about-me.service';
 
 @Component({
   selector: 'app-intro',
@@ -13,6 +16,35 @@ import {APP_CONSTANTS} from '../../../shared';
   templateUrl: './intro.component.html',
   styleUrl: './intro.component.css',
 })
-export class IntroComponent {
+export class IntroComponent implements OnInit, OnDestroy {
+  private sub: Subscription = new Subscription();
+
+  aboutMeData: AboutMeModel = {} as AboutMeModel;
+
   SOCIAL_LINKS = APP_CONSTANTS.socialLinks;
+
+  constructor(
+    private _aboutMeService: AboutMeService,
+  ) {
+  }
+
+  ngOnInit() {
+    this.loadData();
+  }
+
+  ngOnDestroy() {
+    if (this.sub) {
+      this.sub.unsubscribe();
+    }
+  }
+
+  private loadData(): void {
+    this.sub.add(
+      this._aboutMeService.getAboutMe().subscribe(
+        res => {
+          this.aboutMeData = res;
+        }
+      )
+    )
+  }
 }
